5d212e982823aaace7c9e6c4009f0674e12561b3
SawaguchiYuya <166583872+34052SawaguchiYuya@users.noreply.github.com>
2024-05-15 15:11:21 +0900

    3.1.1～3.1.4

---
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //3.1.1
+            Exrcise1_1(number)
+            Console.WriteLine("----");
+            //3.1.2
+            Exrcise1_2(number)
+            Console.WriteLine("----");
+            //3.1.3
+            Exrcise1_3(number)
+            Console.WriteLine("----");
+
+            //3.1.4
+            Exrcise1_4(number)
+            Console.WriteLine("----");
+        }
+        private static void Exerise1_1(List<int> numbers) {
+        }
+
+        private static void Exerise1_2(List<int> numbers) {
+        }
+
+        private static void Exerise1_3(List<int> numbers) {
+        }
+
+        private static void Exerise1_4(List<int> numbers) {
+        }
+    }
+}
