ce261429571d945b771c8dcc0358cf9a43cdd94d
SawaguchiYuya <166583872+34052SawaguchiYuya@users.noreply.github.com>
2024-05-30 13:23:44 +0900

    5-1完了

---
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class YearMonth {
+        //4.1.1
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+        public YearMonth(int year,int month) {
+        int Year = year;
+        int Month = month;
+        }
+        //4.1.2
+        public bool Is21Century {
+            get {
+                return 2001 <= Year && Year <= 2100;/*********/
+            }
+        }
+
+        //4.1.3
+        public YearMonth AddOneMonth() {
+            return new YearMonth(Year, Month +1);
+            if (Month < 12) {
+                Month + 1:
+            } else {
+                Month - 11;
+            }
+        }
+
+        //4.1.4
+        public override string ToString() {
+            return "〇〇〇〇〇";
+        }
+    }
+}
---
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2020, 12),
+            };
+
+        Exercise2_2(ymCollection);
+
+        Exercise2_2(ymCollection);
+
+        Exercise2_2(ymCollection);
+
+        Exercise2_4(ymCollection);
+
+        Exercise2_5(ymCollection);
+    }
+
+    private static void Exercise2_2(YearMonth[] ymCollection) {
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+
+            private static void Exercise2_2C(YearMonth[] yms) {
+
+                private static void Exercise2_4(string text) {
+
+                    private static void Exercise2_5(string text) {
+                    }
+                }
+            }
+        }
