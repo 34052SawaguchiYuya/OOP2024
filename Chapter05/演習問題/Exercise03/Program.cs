c87072590c8d4c9986209df083dd0bb52454ad37
SawaguchiYuya <166583872+34052SawaguchiYuya@users.noreply.github.com>
2024-06-05 10:23:39 +0900

    5-4完了

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -7,34 +7,15 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
-            new YearMonth(1980, 1),
-                new YearMonth(1990, 4),
-                new YearMonth(2000, 7),
-                new YearMonth(2010, 9),
-                new YearMonth(2020, 12),
-            };
 
-        Exercise2_2(ymCollection);
-
-        Exercise2_2(ymCollection);
-
-        Exercise2_2(ymCollection);
-
-        Exercise2_4(ymCollection);
-
-        Exercise2_5(ymCollection);
-    }
-
-    private static void Exercise2_2(YearMonth[] ymCollection) {
-
-        private static void Exercise2_2(YearMonth[] ymCollection) {
-
-            private static void Exercise2_2C(YearMonth[] yms) {
-
-                private static void Exercise2_4(string text) {
-
-                    private static void Exercise2_5(string text) {
-                    }
-                }
-            }
         }
+
+        private static void Exercise3_2(string text)
+
+            private static void Exercise3_2(string text)
+
+            private static void Exercise3_2(string text)
+
+            private static void Exercise3_2(string text)
+    }
+}
