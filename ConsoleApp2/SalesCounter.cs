4109285898d51cd87328c6e5421a1434c6cdb8a7
SawaguchiYuya <166583872+34052SawaguchiYuya@users.noreply.github.com>
2024-06-18 09:45:48 +0900

    salesの完了

--- ConsoleApp2/SalesCounter.cs
+++ ConsoleApp2/SalesCounter.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,25 +9,28 @@ namespace SalesCounter {
     public class SalesCounter {
         private List<Sale> _sales;
 
-        public static string ShopName { get; private set; }
-
         //コンストラクタ
         public SalesCounter(string filePass) {
             _sales = ReadSales(filePass);
         }
 
         //売上データを読み込み、Saleオブジェクトのリストを返す
-        private static IEnumerable<Sale> ReadSales(string filePath) {
+        public static List<Sale> ReadSales(string filePath) {
             List<Sale> sales = new List<Sale>();
             string[] lines = File.ReadAllLines(filePath);
             foreach (string line in lines) {
                 string[] items = line.Split(',');
-                Sale sale = new Sale();
-                ShopName = items[0];
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2]),
+                };
+                sales.Add(sale);
             }
+            return sales;
         }
 
-        private static IDictionary<string, int> GetPerStoreSales() {
+        private IDictionary<string, int> GetPerStoreSales() {
             var dict = new Dictionary<string, int>();
             foreach (var sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName)) {
