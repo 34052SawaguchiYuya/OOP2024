4a47fdbf94d9c1e0ece2f5d6e784a29b254d7380
TakahashiTaiki <oibc35150@ojs.ac.jp>
2024-10-31 12:10:57 +0900

    10/31

---
+++ WPF/SampleUnitConverter/Model/ImperialUnit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitConverter {
+    //メートル単位を表すクラス
+    public class ImperialUnit : DistanceUnit {
+        private static List<ImperialUnit> units = new List<ImperialUnit> {
+            new ImperialUnit{Name = "in",Coefficients = 1,},
+            new ImperialUnit{Name = "ft",Coefficients = 12,},
+            new ImperialUnit{Name = "yd",Coefficients = 12 * 3,},
+            new ImperialUnit{Name = "ml",Coefficients = 12 * 3 * 1760,},
+        };
+        public static ICollection<ImperialUnit> Units { get { return units; } }
+
+
+        public double FromImperialUnit(ImperialUnit unit, double value) {
+            return (value * unit.Coefficients) * 25.4 / this.Coefficients;
+        }
+    }
+}
+    /*public class ImperialUnit : DistanceUnit {
+        private static List<ImperialUnit> units = new List<ImperialUnit> {
+            new ImperialUnit{Name = "in",Coefficients = 1,},
+            new ImperialUnit{Name = "ft",Coefficients = 1,},
+            new ImperialUnit{Name = "yd",Coefficients = 1,},
+            new ImperialUnit{Name = "ml",Coefficients = 1,},
+        };
+        public static ICollection <ImperialUnit> Units { get { return units; } }
+    }
+}*/
---
+++ WPF/SampleUnitConverter/Model/MetricUnit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitConverter {
+    //メートル単位を表すクラス
+    public class MetricUnit : DistanceUnit {
+        private static List<MetricUnit> units = new List<MetricUnit> {
+            new MetricUnit{Name = "mm",Coefficients = 1,},
+            new MetricUnit{Name = "mm",Coefficients = 10,},
+            new MetricUnit{Name = "mm",Coefficients = 10 * 100,},
+            new MetricUnit{Name = "mm",Coefficients = 10 * 100 * 1000,},
+        };
+        public static ICollection<MetricUnit> Units { get { return units; } }
+
+        public double FromImperialUnit(ImperialUnit unit, double value) {
+            return (value * unit.Coefficients) * 25.4 / this.Coefficients;
+        }
+    }
+}
+    /*public class ImperialUnit : DistanceUnit {
+        private static List<ImperialUnit> units = new List<ImperialUnit> {
+            new ImperialUnit{Name = "in",Coefficients = 1,},
+            new ImperialUnit{Name = "ft",Coefficients = 1,},
+            new ImperialUnit{Name = "yd",Coefficients = 1,},
+            new ImperialUnit{Name = "ml",Coefficients = 1,},
+        };
+        public static ICollection <ImperialUnit> Units { get { return units; } }
+    }
+}*/
